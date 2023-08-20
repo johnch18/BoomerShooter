48f2e25255eb72e4abdc32dc58510442e19bc068
Charles Johnson <johnch18@isu.edu>
2023-08-20 14:35:09 -0600

    temp

--- src/main/java/com/johnch18/boomer/render/CenteredItemRender.java
+++ src/main/java/com/johnch18/boomer/render/CenteredItemRender.java
@@ -1,5 +1,6 @@
 package com.johnch18.boomer.render;
 
+
 import net.minecraft.client.renderer.ItemRenderer;
 import net.minecraft.client.renderer.Tessellator;
 import net.minecraft.item.ItemStack;
@@ -9,6 +10,11 @@ import org.lwjgl.opengl.GL11;
 
 
 public class CenteredItemRender implements IItemRenderer {
+
+    protected static void standardRendering(final Tessellator tessellator, final IIcon icon) {
+        ItemRenderer.renderItemIn2D(
+                tessellator, icon.getMaxU(), icon.getMinV(), icon.getMinU(), icon.getMaxV(), icon.getIconWidth(), icon.getIconHeight(), 0.0625f);
+    }
 
     @Override
     public boolean handleRenderType(ItemStack item, ItemRenderType type) {
@@ -25,10 +31,40 @@ public class CenteredItemRender implements IItemRenderer {
         final IIcon icon = item.getItem().getIcon(item, 0);
         final Tessellator tessellator = Tessellator.instance;
         //
+        glPre();
+        //
+        doRendering(tessellator, icon);
+        //
+        glPost();
+    }
+
+    protected static void glPre() {
+
         GL11.glPushMatrix();
+        GL11.glPushAttrib(GL11.GL_ALL_ATTRIB_BITS);
+        GL11.glLoadIdentity();
+        GL11.glScalef(0.5f, 0.5f, 0.5f);
+
+    }
+
+    protected void doRendering(final Tessellator tessellator, final IIcon icon) {
+        final double u, U, v, V;
+        u = icon.getMinU();
+        U = icon.getMaxU();
+        v = icon.getMinV();
+        V = icon.getMaxV();
         //
-        ItemRenderer.renderItemIn2D(tessellator, icon.getMaxU(), icon.getMinV(), icon.getMinU(), icon.getMaxV(), icon.getIconWidth(), icon.getIconHeight(), 0.0625f);
-        //
+        tessellator.startDrawingQuads();
+        tessellator.setNormal(0.0f, 0.0f, 1.0f);
+        tessellator.addVertexWithUV(0.0, 0.0, 0.0, U, V);
+        tessellator.addVertexWithUV(1.0, 0.0, 0.0, u, V);
+        tessellator.addVertexWithUV(1.0, 1.0, 0.0, u, v);
+        tessellator.addVertexWithUV(0.0, 1.0, 0.0, U, v);
+        tessellator.draw();
+    }
+
+    protected static void glPost() {
+        GL11.glPopAttrib();
         GL11.glPopMatrix();
     }
 
